c4450f2e9a52d01713fe0ec93ec1fc8093e6a93c
Gregory Jones <greggie119gj@gmail.com>
2025-05-09 21:09:04 -0400

    Set AC to red if colliding or outside vert. airspace

--- bluesky_gym/envs/static_obstacle_env_v1.py
+++ bluesky_gym/envs/static_obstacle_env_v1.py
@@ -412,8 +412,15 @@ class StaticObstacleEnvMod(gym.Env):
         dis = dis*NM2KM
         x_actor = ((np.sin(np.deg2rad(qdr))*dis)/MAX_DISTANCE)*self.window_width
         y_actor = ((-np.cos(np.deg2rad(qdr))*dis)/MAX_DISTANCE)*self.window_width
+
+        # set colors of ship based on intrusion
+        _, terminate = self._check_intrusion()
+
+        color = (0,0,0) # black
+        if terminate:
+            color = (235, 52, 52) # red
         pygame.draw.line(canvas,
-            (235, 52, 52),
+            color,
             (x_actor, y_actor),
             (x_actor+heading_end_x, y_actor-heading_end_y),
             width = 5
